ac53618bbe897c8972dc8b3a3deb417277d2df92
Ali Amaan <aliamaan071@gmail.com>
2021-10-07 01:01:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const billAmount = document.querySelector('#bill-amount')
-const cashGiven = document.querySelector('#cash-given')
-const checkButton = document.querySelector('#check-button')
-const message = document.querySelector('#error-message')
-const noOfNotes = document.querySelectorAll('.no-of-notes')
+const billAmount = document.querySelector("#bill-amount")
+const cashGiven = document.querySelector("#cash-given")
+const checkButton = document.querySelector("#check-button")
+const message = document.querySelector("#error-message")
+const noOfNotes = document.querySelectorAll(".no-of-notes")
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
 
-checkButton.addEventListener('click', function validateBillAndCashAmount() {
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage()
   const cashGivenAmt = parseInt(cashGiven.value)
   const billAmt = parseInt(billAmount.value)
-  if (billAmt > 0) {
+  if (billAmt !== cashGivenAmt && billAmt > 0) {
     // 12
     if (cashGivenAmt >= billAmt) {
       // 2022> 12 => true
       const amountToBeReturned = cashGivenAmt - billAmt // 2022 - 12 = 2010
       calculateChange(amountToBeReturned)
     } else {
-      showMessage('Do you wanna wash plates?')
+      showMessage("Do you wanna wash plates?")
     }
+  } else if (billAmt === cashGivenAmt) {
+    showMessage("No Change to return.")
   } else {
-    showMessage('Invalid Bill Amount')
+    showMessage("Invalid Bill Amount")
   }
 })
 
@@ -33,10 +35,10 @@ function calculateChange(amountToBeReturned) {
 }
 
 function hideMessage() {
-  message.style.display = 'none'
+  message.style.display = "none"
 }
 
 function showMessage(msg) {
-  message.style.display = 'block'
+  message.style.display = "block"
   message.innerText = msg
 }
